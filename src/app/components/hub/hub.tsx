98e4bb8ec8fa4441abc9fc61939146c5a3b712ed
jessi <155872918+jessivxi@users.noreply.github.com>
2025-06-27 14:39:14 -0300

    deletando fotos não usadas

--- src/app/components/hub/hub.tsx
+++ src/app/components/hub/hub.tsx
@@ -15,7 +15,6 @@ import artedigital from '@/images/fotos/arte_digital.jpg'
 function Hub() {
     return (
         <>
-        <Header/>
             <div className={styles.main}>
         <div className={styles.searchBarMain}>
             <Image src={gato_arroba} height={100} width={100} alt="gatobonito"></Image> 

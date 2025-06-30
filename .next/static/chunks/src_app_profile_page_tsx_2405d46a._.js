2a805f588a6c29f78ce9065b7d9d0f0a50048121
jessi <155872918+jessivxi@users.noreply.github.com>
2025-06-30 16:07:08 -0300

    atualizando

---
+++ .next/static/chunks/src_app_profile_page_tsx_2405d46a._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_app_profile_profile_module_fe063259.css"
+],
+    source: "dynamic"
+});
